ccf4acca5430ec967754fa00897c9fb2e0eb94c5
Maks <maks@Air-Maks.Dlink>
2021-11-07 00:39:58 +0300

    completed the task

---
+++ 8 kyu Correct the mistakes of the character recognition software.js
@@ -0,0 +1,13 @@
+function correct(string) {
+    const corrections = {
+    "5": "S",
+    "0": "O",
+    "1": "I",
+  };
+
+  return (
+    string.split("").map((char) =>
+        corrections.hasOwnProperty(char) ? corrections[char] : char
+      ).join("")
+  );
+}
