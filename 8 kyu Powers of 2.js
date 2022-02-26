6bc215be712bc57f63f7885be5d8c2a651ae3399
Belom7 <Still07pkm@gmail.com>
2022-02-26 13:17:16 +0300

    completed the task

--- 8 kyu Powers of 2.js
+++ 8 kyu Powers of 2.js
@@ -1,6 +1,6 @@
 function powersOfTwo(n){
-    var result = [];
-    for (var i = 0; i <= n; i++) {
+    let result = [];
+    for (let i = 0; i <= n; i++) {
         result.push(Math.pow(2, i));
     }
     return result;
