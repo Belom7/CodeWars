8be1d48b18532e20cd57b62f8d4b6bbdd515763f
Belom7 <Still07pkm@gmail.com>
2022-03-24 11:44:00 +0300

    completed the task

---
+++ 7 kyu Character Counter.js
@@ -0,0 +1,8 @@
+function validateWord(s){
+    let counts = {};
+    for(let c of s.toLowerCase()){
+        if(counts[c] === undefined){counts[c] = 0;}
+        counts[c]++;
+    }
+    return Math.max(...Object.values(counts)) === Math.min(...Object.values(counts));
+}
