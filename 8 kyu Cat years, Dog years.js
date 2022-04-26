c04da443b8bcda5a8511adac8d1311c1413b9594
Belom7 <Still07pkm@gmail.com>
2022-04-26 22:29:54 +0300

    completed the task

---
+++ 8 kyu Cat years, Dog years.js
@@ -0,0 +1,5 @@
+let humanYearsCatYearsDogYears = function (humanYears) {
+    return humanYears === 1 ? [1, 15, 15]
+        : humanYears === 2 ? [2, 24, 24]
+            : [humanYears, 24 + (4 * (humanYears - 2)), 24 + (5 * (humanYears - 2))];
+}
