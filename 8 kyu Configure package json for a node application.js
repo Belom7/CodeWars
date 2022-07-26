bee9cc61b9ec5d17408cef839ad1c89acf43d970
Belom7 <Still07pkm@gmail.com>
2022-07-26 23:06:20 +0300

    completed the task

---
+++ 8 kyu Configure package json for a node application.js
@@ -0,0 +1,20 @@
+const configuration = {
+    "name": "my-project",
+    "version": "15.10.2",
+    "description": "very-very-project",
+    "main": "main.js",
+    "scripts": {
+        "start": "react-scripts start",
+        "build": "react-scripts build",
+        "test": "react-scripts test",
+        "eject": "react-scripts eject"
+    },
+    "keywords": ['abc','cdv'],
+    "bugs": {"url" : "abracadabra@mail.ru" },
+    "author": "Maks, abracadabra@mail.ru, www.mail.ru",
+    "private": true,
+    "preferGlobal": true,
+    "os": ['macOS', 'Windows'],
+    "cpu": ['celeron','amd'],
+
+};
