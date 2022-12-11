436ca1601aa43d9e2c81014b0313479b6f196e5f
Belom7 <Still07pkm@gmail.com>
2022-12-11 23:06:04 +0300

    completed the task

---
+++ 8 kyu Count the number of cubes with paint on.js
@@ -0,0 +1,10 @@
+const countSquares = function(cuts){
+    if (!cuts) {
+        return 1;
+    }
+
+    const totalCubes = Math.pow(cuts + 1, 3);
+    const innerCubes = Math.pow(cuts - 1, 3);
+
+    return totalCubes - innerCubes;
+}
