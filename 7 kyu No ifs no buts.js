048fae1976b45218458a07f7f3698c761c0abcec
Belom7 <Still07pkm@gmail.com>
2023-04-27 07:34:57 +0300

    completed the task

---
+++ 7 kyu No ifs no buts.js
@@ -0,0 +1,7 @@
+var noIfsNoButs = function (a,b) {
+    switch (true) {
+        case (a <   b) : return a + " is smaller than " + b;
+        case (a >   b) : return a + " is greater than " + b;
+        default        : return a + " is equal to "     + b;
+    }
+}
