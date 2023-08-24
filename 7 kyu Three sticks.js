52c744ee729fe525bf3e58352562a467541ae6e5
Belom7 <Still07pkm@gmail.com>
2023-08-24 20:38:15 +0300

    completed the task

---
+++ 7 kyu Three sticks.js
@@ -0,0 +1,13 @@
+function maxlen(l1, l2) {
+    // return maximum length
+    let max = Math.max(l1, l2)
+    let min = Math.min(l1, l2)
+
+    if(max / 3 > min)
+        return max/3
+
+    if(max - min > min)
+        return min
+
+    return max/2
+}
