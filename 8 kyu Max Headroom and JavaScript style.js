6ad7ad38ec8dd61e5bbe449a177a2fedfe2741bf
Belom7 <Still07pkm@gmail.com>
2024-02-21 22:52:12 +0300

    completed the task

---
+++ 8 kyu Max Headroom and JavaScript style.js
@@ -0,0 +1,13 @@
+function getMax1(){
+    let max = {
+        name: 'Max Headroom'
+    }
+    return max;
+}
+
+function getMax2(){
+    let max = {
+        name: 'Max Headroom'
+    }
+    return max
+}
