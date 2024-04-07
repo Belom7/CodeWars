dd621438e5c9af126aae9b91d1763e3a8f691b83
Belom7 <Still07pkm@gmail.com>
2024-04-07 22:11:05 +0300

    completed the task

---
+++ 7 kyu [JS] Parse integers in array.js
@@ -0,0 +1,8 @@
+/**
+ * Function converts array of string to array of integers.
+ * @param  {Array} of numbers in form of string
+ * @return {Array} of integer numbers
+ */
+const parseNumbers = function (intStrs) {
+    return intStrs.map((num) => parseInt(num));
+};
