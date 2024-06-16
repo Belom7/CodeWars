37341f2a965a8524857ae26d6d42d19c79050165
Belom7 <Still07pkm@gmail.com>
2024-06-16 21:54:08 +0300

    completed the task

---
+++ 7 kyu Error Throwing - Error Handling #2.js
@@ -0,0 +1,7 @@
+const validateMessage = msg => {
+    if (msg === null) throw ReferenceError('Message is null!');
+    if (typeof msg != 'string') throw TypeError(`Message should be of type string but was of type ${typeof msg}!`);
+    if (!msg.length || msg.length > 255) throw RangeError(`Message contains ${msg.length} characters!`);
+    if (/<.*>/.test(msg)) return false;
+    return true;
+}
