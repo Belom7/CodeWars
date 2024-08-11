07a39ebe6064bf32d837fcbe9868320baa4fd7a9
Belom7 <Still07pkm@gmail.com>
2024-08-11 23:59:27 +0300

    completed the task

---
+++ 7 kyu Doggy Daycare.js
@@ -0,0 +1,5 @@
+Dog.prototype.checkDog = function() {
+    if (this.vaccinated && this.wormed) return `${ this.name } can be accepted`;
+    if (this.vaccinated || this.wormed) return `${ this.name } can only be accepted by itself`;
+    return `${ this.name } can not be accepted`;
+};
