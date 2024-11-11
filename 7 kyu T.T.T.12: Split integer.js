2499081232197eabf377ad257002505f3677a6e4
Belom7 <Still07pkm@gmail.com>
2024-11-11 23:24:00 +0300

    completed the task

---
+++ 7 kyu T.T.T.12: Split integer.js
@@ -0,0 +1,7 @@
+function splitInt(n) {
+    if(n==2) return 1
+    if(n==3) return 2
+    if(n%3==0) return Math.pow(3, n/3)
+    if(n%3==2) return 2 * Math.pow(3, (n-2)/3)
+    if(n%3==1) return 4 * Math.pow(3, (n-4)/3)
+}
